f482cd6ac601fd2c08a93a568e7d0524136dc9e6
Aaron Evans <aarone@outlook.com.au>
2025-03-03 12:52:34 +1100

    Bump landing zone module version to 1.1.1

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 module "landingzone" {
   source  = "app.terraform.io/cloudbrokeraz/landingzone/aws"
-  version = "1.1.0"
+  version = "1.1.1"
 
   # Feature flags
   enable_vpc         = var.enable_vpc
